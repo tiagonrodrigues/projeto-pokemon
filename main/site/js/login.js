414e5ec87473acdd2f6ed6fc84a767433b9a6aee
Henrique Ramos <57632140+henriquenow@users.noreply.github.com>
2021-04-11 17:30:09 +0100

    Verificação do login em JS

---
+++ main/site/js/login.js
@@ -0,0 +1,23 @@
+var tentativa = 3; 
+
+function validar(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if ( username == "user" && password == "1234"){
+        alert ("Login com sucesso!!");
+        //window.location = "success.html";
+        return false;
+    }
+    else{
+        tentativa --;
+        alert("Ainda tens "+tentativa+" tentativas");
+    
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
