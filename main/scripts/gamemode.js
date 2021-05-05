497e648c1341ce3d203310b4cbe8a5331eb81a7e
Henrique Ramos <57632140+henriquenow@users.noreply.github.com>
2021-05-05 19:02:33 +0100

    Abrir modos de jogo

--- main/scripts/gamemode.js
+++ main/scripts/gamemode.js
@@ -6,3 +6,16 @@ function favTutorial() {
   var mylist = document.getElementById("myList");  
   localStorage.setItem("modo", mylist.options[mylist.selectedIndex].text );  
   }  
+
+function mode(){
+  var mode = localStorage.getItem("modo");
+  if(mode == "Speedrun"){
+    window.open("../pages/speedrun.html");
+  }
+  if(mode == "Ninja"){
+    window.open("../pages/ninja.html");
+  }
+  if(mode == "The Conqueror"){
+    window.open("../pages/conqueror.html");
+  }
+}
