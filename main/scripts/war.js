a5e081fb2056820b0045f883677a135f92d99d40
Tiago Rodrigues <78546684+tiagonrodrigues@users.noreply.github.com>
2021-05-05 23:42:57 +0100

    reorganized code

--- main/scripts/war.js
+++ main/scripts/war.js
@@ -3,8 +3,8 @@ var dice = {
   roll: function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
-  }
-}
+  },
+};
 
 function roll_dice() {
   var my_roll = dice.roll();
@@ -24,12 +24,12 @@ function roll_dice() {
     alert("GAME OVER!");
     localStorage.setItem("time", document.getElementById("timer").innerHTML);
     window.open("../pages/gamemode.html");
-
-  }
-  else {
+  } else {
     document.getElementById("myHP").innerHTML = new_myHP;
-    document.getElementById("my_message").innerHTML = "Your attack took " + my_damage + result2;
-    document.getElementById("ap_message").innerHTML = "You lost " + ap_damage + result;
+    document.getElementById("my_message").innerHTML =
+      "Your attack took " + my_damage + result2;
+    document.getElementById("ap_message").innerHTML =
+      "You lost " + ap_damage + result;
   }
   if (new_apHP <= 0) {
     document.getElementById("apHP").innerHTML = 0;
@@ -42,19 +42,22 @@ function roll_dice() {
     document.getElementById("war").hidden = true;
     document.getElementById("myHP").innerHTML = 100;
     document.getElementById("apHP").innerHTML = 100;
-  }
-  else {
+  } else {
     document.getElementById("apHP").innerHTML = new_apHP;
-    document.getElementById("my_message").innerHTML = "Your attack took " + my_damage + result2;
-    document.getElementById("ap_message").innerHTML = "You lost " + ap_damage + result;
+    document.getElementById("my_message").innerHTML =
+      "Your attack took " + my_damage + result2;
+    document.getElementById("ap_message").innerHTML =
+      "You lost " + ap_damage + result;
   }
 
-  var reddmg = " Damage"
-  var result2 = reddmg.fontcolor("#FF0000")
+  var reddmg = " Damage";
+  var result2 = reddmg.fontcolor("#FF0000");
 
   var greenhp = " HP";
   var result = greenhp.fontcolor("#33FF33");
 
-  document.getElementById("my_message").innerHTML = "Your attack took " + my_damage + result2;
-  document.getElementById("ap_message").innerHTML = "You lost " + ap_damage + result;
+  document.getElementById("my_message").innerHTML =
+    "Your attack took " + my_damage + result2;
+  document.getElementById("ap_message").innerHTML =
+    "You lost " + ap_damage + result;
 }
