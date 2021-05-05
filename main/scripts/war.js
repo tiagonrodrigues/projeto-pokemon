70d1b47cc7424ff77b8673fdd70814d85219c129
Tiago Rodrigues <78546684+tiagonrodrigues@users.noreply.github.com>
2021-05-05 19:33:39 +0100

    colorized dmg and hp

--- main/scripts/war.js
+++ main/scripts/war.js
@@ -39,6 +39,12 @@ function roll_dice(){
         document.getElementById("apHP").innerHTML = new_apHP;
     }
 
-    document.getElementById("my_message").innerHTML = "Your attack took " + my_damage + " damage!";
-    document.getElementById("ap_message").innerHTML = "You lost " + ap_damage + "HP";
+    var greenhp = " HP";
+    var result = greenhp.fontcolor("#33FF33");
+
+    var reddmg = " Damage"
+    var result2 = reddmg.fontcolor("#FF0000")
+
+    document.getElementById("my_message").innerHTML = "Your attack took " + my_damage + result2;
+    document.getElementById("ap_message").innerHTML = "You lost " + ap_damage + result;
 }
