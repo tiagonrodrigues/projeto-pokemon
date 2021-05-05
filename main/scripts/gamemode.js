e965d6770cbff4cb492478e7e6487fc03bc2eab2
Henrique Ramos <57632140+henriquenow@users.noreply.github.com>
2021-05-05 13:12:16 +0100

    Simplificar code

--- main/scripts/gamemode.js
+++ main/scripts/gamemode.js
@@ -1,7 +1,5 @@
 function username() {
-  var Local_user = localStorage.getItem("username");
-
-  document.getElementById("welcome").innerHTML = "Welcome back, " + Local_user + "!";
+  document.getElementById("welcome").innerHTML = "Welcome back, " + localStorage.getItem("username") + "!";
 }
 
 function favTutorial() {  
